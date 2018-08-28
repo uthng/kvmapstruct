6eff2acce8cc517ec286fe7d7817b35523b1f673
uthng <u.nguyenthanh@gmail.com>
2018-08-28 22:20:46 +0200

    - Add several functions to map a map to struct: MapToFlattenMap,   KVMapToMap, KVMapToStruct, etc. - Add or modify tests

--- kvstruct.go
+++ kvstruct.go
@@ -1,9 +1,12 @@
 package kvstruct
 
 import (
-	//"encoding/base64"
 	"fmt"
 	"reflect"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
 
 	"github.com/fatih/structs"
 	consul "github.com/hashicorp/consul/api"
@@ -88,6 +91,12 @@ func (ks *KVStruct) StructToConsulKV(input interface{}) error {
 	return nil
 }
 
+// KVPairsToStruct gets list of all consul keys, remove prefix if set,
+// and match them to out struct or map
+func (ks *KVStruct) KVPairsToStruct(in consul.KVPairs, out interface{}) error {
+	return nil
+}
+
 // MapToKVPairs convert a map to a flatten kv pairs
 func (ks *KVStruct) MapToKVPairs(in map[string]interface{}, prefix string) (consul.KVPairs, error) {
 	var out consul.KVPairs
@@ -107,7 +116,7 @@ func (ks *KVStruct) MapToKVPairs(in map[string]interface{}, prefix string) (cons
 	return out, nil
 }
 
-// MapToKVMap convert a map to a flatten kv list
+// MapToKVMap convert a map to a flatten kv list handling slice & array
 func MapToKVMap(in map[string]interface{}, prefix string) map[string]interface{} {
 	out := make(map[string]interface{})
 	key := ""
@@ -146,6 +155,294 @@ func MapToKVMap(in map[string]interface{}, prefix string) map[string]interface{}
 	}
 
 	return out
+}
+
+// MapToFlattenMap converts a nested map to a flatten map keeping slice & array
+// Only the following types: int, bool, string and map[string]interface{} are supported.
+func MapToFlattenMap(in map[string]interface{}, prefix string) map[string]interface{} {
+	out := make(map[string]interface{})
+	key := ""
+
+	if prefix != "" {
+		key = prefix + "/"
+	}
+
+	// Loop map to build
+	for k, v := range in {
+		kind := reflect.ValueOf(v).Kind()
+		if kind == reflect.Map {
+			o := MapToFlattenMap(v.(map[string]interface{}), key+k)
+			if len(o) <= 0 {
+				out[key+k] = o
+			} else {
+				for k1, v1 := range o {
+					out[k1] = v1
+				}
+			}
+		} else if kind == reflect.Slice {
+			// TODO: Maybe there is another way to do this more elegant
+			switch v.(type) {
+			case []int:
+				out[key+k] = v.([]int)
+			case []string:
+				out[key+k] = v.([]string)
+			case []bool:
+				out[key+k] = v.([]bool)
+			}
+		} else {
+			out[key+k] = v
+		}
+	}
+
+	return out
+}
+
+// KVMapToStruct converts a key map to struct.
+// Only the following types: int, bool, string and map[string]interface{} are supported.
+func KVMapToStruct(in map[string]interface{}, prefix string, out interface{}) error {
+	var inVal interface{}
+
+	v := reflect.ValueOf(out)
+	// Get value of pointer
+	indirect := reflect.Indirect(v)
+	k := v.Kind()
+
+	// Check if out is a pointer to a structure
+	if k != reflect.Ptr || indirect.Kind() != reflect.Struct {
+		return fmt.Errorf("Error of output's type! Only pointer of struct are supported")
+	}
+
+	// If struct, convert it to Map
+	flattenOut := structs.Map(out)
+
+	for k, v := range flattenOut {
+		val := reflect.ValueOf(v)
+		kind := val.Kind()
+
+		// Initialize inVal
+		inVal = nil
+
+		// If value is not a slice or a map, we assign value directly
+		if kind == reflect.Slice {
+			i := 0
+			arr := []string{}
+			// Loop by incremnenting i to get all values of slice
+			for {
+				v1, ok := in[k+"/"+cast.ToString(i)]
+				if !ok {
+					break
+				}
+				arr = append(arr, cast.ToString(v1))
+				i = i + 1
+			}
+
+			inVal = arr
+		} else if kind == reflect.Map {
+			// Convert kv map to a nested map with prefix that is the key
+			m, err := KVMapToMap(in, k)
+			if err != nil {
+				return err
+			}
+
+			inVal = m
+
+		} else {
+			// Check in kvmap
+			inVal = in[k]
+		}
+
+		// Assign value following its type
+		if inVal != nil {
+			switch v.(type) {
+			case int:
+				flattenOut[k] = cast.ToInt(inVal)
+			case bool:
+				flattenOut[k] = cast.ToBool(inVal)
+			case string:
+				flattenOut[k] = cast.ToString(inVal)
+			case []int:
+				flattenOut[k] = cast.ToIntSlice(inVal)
+			case map[string]interface{}:
+				flattenOut[k] = inVal
+			default:
+				return fmt.Errorf("error type at key %s", k)
+			}
+		}
+	}
+
+	// Convert struct's flatten map to struct
+	err := FlattenMapToStruct(flattenOut, prefix, out)
+	//fmt.Println(err, out)
+	return err
+}
+
+// KVMapToMap converts a flatten kv pairs map to nested map
+// In KVMap, a  slice is represented by keys appended by integers such as key/0, key/1, key/2 etc.
+// KVMapToMap tries to rebulild and respect slice type. But only common types such as
+// int, bool, string and map[string]interface{} are supported
+func KVMapToMap(in map[string]interface{}, prefix string) (map[string]interface{}, error) {
+	var keys []string
+	out := make(map[string]interface{})
+	key := ""
+	parent := ""
+	count := 1
+	slice := false
+
+	// Create a slice only containing in map's keys to be sorted
+	for k := range in {
+		keys = append(keys, k)
+	}
+
+	sort.Strings(keys)
+
+	// Loop sorted map
+	for _, k := range keys {
+		key = k
+
+		// If prefix is set, check if key contains prefix and remove it
+		if prefix != "" {
+			// Only handle key containing prefix, zap others
+			re := regexp.MustCompile(prefix + "/.*")
+			if re.MatchString(key) {
+				key = strings.Replace(key, prefix+"/", "", 1)
+			} else {
+				continue
+			}
+		}
+
+		// Assign current out
+		outchilds := out
+
+		// Split / to create submap
+		childs := strings.Split(key, "/")
+		if len(childs) > 0 {
+			// Get the last key that will be assigned a value
+			key = childs[len(childs)-1]
+
+			// Check if key is an elem of slice(0, 1, 3 etc.)
+			pos, err := strconv.Atoi(key)
+			if err == nil && pos == count {
+				// Get parent of key ==> slice field
+				parent = childs[len(childs)-2]
+				slice = true
+				count = count + 1
+			} else {
+				// Reinitialize variables for slice case
+				slice = false
+				count = 0
+				parent = ""
+			}
+
+			// In case of slice, remove key + its parents (slice itself)
+			// Otherwise, remove only the last key
+			if slice {
+				childs = childs[:len(childs)-2]
+			} else {
+				childs = childs[:len(childs)-1]
+			}
+
+			for _, child := range childs {
+				// Check if key exists already. If not, create a new map
+				if outchilds[child] == nil {
+					outchilds[child] = make(map[string]interface{})
+				}
+
+				// Get the child if it exists. If not return an error
+				subchild, ok := outchilds[child].(map[string]interface{})
+				if !ok {
+					return nil, fmt.Errorf("child is both a data item and dir: %s", child)
+				}
+
+				// Assign subchild to outchilds to do recursively
+				outchilds = subchild
+			}
+
+			// Assign value to the last key
+			// In case of slice, if 1st elem, check type of slice elem value
+			// to initialize slice with the same type. Otherwise add simply elem to slice
+			if slice {
+				val := in[k]
+				switch val.(type) {
+				case int:
+					if pos == 0 {
+						outchilds[parent] = []int{}
+					}
+					outchilds[parent] = append(outchilds[parent].([]int), val.(int))
+				case string:
+					if pos == 0 {
+						outchilds[parent] = []string{}
+					}
+					outchilds[parent] = append(outchilds[parent].([]string), val.(string))
+				case bool:
+					if pos == 0 {
+						outchilds[parent] = []bool{}
+					}
+					outchilds[parent] = append(outchilds[parent].([]bool), val.(bool))
+				default:
+					return out, fmt.Errorf("Type error! Only int, string, bool are supported")
+				}
+			} else {
+				// Other cases, assign simply value to key
+				outchilds[key] = cast.ToString(in[k])
+			}
+		}
+	}
+
+	return out, nil
+}
+
+// FlattenMapToStruct converts
+// Go struct that is going to be filled up must be a pointer and initialized.
+// If it is a neseted struct, all substructs must be a pointer and get initialized.
+func FlattenMapToStruct(in map[string]interface{}, prefix string, out interface{}) error {
+	if out == nil {
+		return fmt.Errorf("go struct is not initialized")
+	}
+
+	v := reflect.ValueOf(out)
+	// Get value of pointer
+	indirect := reflect.Indirect(v)
+	k := v.Kind()
+
+	// Check if out is a pointer to a structure
+	if k != reflect.Ptr || indirect.Kind() != reflect.Struct {
+		return fmt.Errorf("Error of output's type! Only pointer of struct are supported")
+	}
+
+	for i := 0; i < indirect.Type().NumField(); i++ {
+		field := indirect.Type().Field(i)
+
+		k := field.Type.Kind()
+		v := indirect.FieldByName(field.Name)
+
+		if k == reflect.Ptr && reflect.Indirect(v).Kind() == reflect.Struct {
+			err := FlattenMapToStruct(cast.ToStringMap(in[field.Name]), field.Name, v.Interface())
+			if err != nil {
+				return err
+			}
+		} else {
+			switch t := v.Interface().(type) {
+			case string:
+				v.SetString(cast.ToString(in[field.Name]))
+			case int:
+				v.SetInt(cast.ToInt64(in[field.Name]))
+			case bool:
+				v.SetBool(cast.ToBool(in[field.Name]))
+			case []int:
+				v.Set(reflect.ValueOf(cast.ToIntSlice(in[field.Name])))
+			case []string:
+				v.Set(reflect.ValueOf(cast.ToStringSlice(in[field.Name])))
+			case []bool:
+				v.Set(reflect.ValueOf(cast.ToBoolSlice(in[field.Name])))
+			case map[string]interface{}:
+				v.Set(reflect.ValueOf(cast.ToStringMap(in[field.Name])))
+			default:
+				return fmt.Errorf("type error not supported %s", t)
+			}
+		}
+	}
+
+	return nil
 }
 
 //////////////////////// PRIVATE FUNCTIONS ///////////////////////
--- kvstruct_test.go
+++ kvstruct_test.go
@@ -260,13 +260,13 @@ type ST struct {
 	Key1 string
 	Key2 int
 	Key3 []int
-	Key4 STChildLevel1
+	Key4 *STChildLevel1
 }
 
 type STChildLevel1 struct {
 	Key41 string
 	Key42 map[string]interface{}
-	Key43 STChildLevel2
+	Key43 *STChildLevel2
 }
 
 type STChildLevel2 struct {
@@ -287,13 +287,13 @@ func TestStructToConsulKV(t *testing.T) {
 				Key1: "val1",
 				Key2: 2,
 				Key3: []int{1, 2, 3},
-				Key4: STChildLevel1{
+				Key4: &STChildLevel1{
 					Key41: "val41",
 					Key42: map[string]interface{}{
 						"Key421": "val421",
 						"Key422": []string{"one", "two", "three"},
 					},
-					Key43: STChildLevel2{
+					Key43: &STChildLevel2{
 						Key431: map[string]interface{}{
 							"Key4311": "val4311",
 						},
@@ -355,6 +355,241 @@ func TestStructToConsulKV(t *testing.T) {
 			}
 
 			client.KV().DeleteTree(tc.prefix, nil)
+		})
+	}
+
+}
+
+func TestMapToFlattenMap(t *testing.T) {
+	testCases := []struct {
+		name   string
+		prefix string
+		input  map[string]interface{}
+		output map[string]interface{}
+	}{
+		{
+			"NestedMapWithPrefix",
+			"test",
+			map[string]interface{}{
+				"key1": "val1",
+				"key2": 2,
+				"key3": []int{1, 2, 3},
+				"key4": map[string]interface{}{
+					"key41": "val41",
+					"key42": map[string]interface{}{
+						"key421": "val421",
+						"key422": []string{"one", "two", "three"},
+					},
+					"key43": map[string]interface{}{
+						"key431": map[string]interface{}{
+							"key4311": "val4311",
+						},
+					},
+				},
+			},
+			map[string]interface{}{
+				"test/key1":                      "val1",
+				"test/key2":                      2,
+				"test/key3":                      []int{1, 2, 3},
+				"test/key4/key41":                "val41",
+				"test/key4/key42/key421":         "val421",
+				"test/key4/key42/key422":         []string{"one", "two", "three"},
+				"test/key4/key43/key431/key4311": "val4311",
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			o := MapToFlattenMap(tc.input, tc.prefix)
+
+			if !reflect.DeepEqual(o, tc.output) {
+				t.Errorf("\nwant:\n%s\nhave:\n%s", tc.output, o)
+			}
+		})
+	}
+
+}
+
+func TestKVMapToMap(t *testing.T) {
+	testCases := []struct {
+		name   string
+		prefix string
+		input  map[string]interface{}
+		output map[string]interface{}
+	}{
+		{
+			"NestedMapWithPrefix",
+			"test",
+			map[string]interface{}{
+				"test/key1":                "val1",
+				"test/key2":                2,
+				"test/key3/0":              1,
+				"test/key3/1":              2,
+				"test/key3/2":              3,
+				"test/key4/key41":          "val41",
+				"test/key4/key42/key421":   "val421",
+				"test/key4/key42/key422/0": "one",
+				"test/key4/key42/key422/1": "two",
+				"test/key4/key42/key422/2": "three",
+			},
+			map[string]interface{}{
+				"key1": "val1",
+				"key2": "2",
+				"key3": []int{1, 2, 3},
+				"key4": map[string]interface{}{
+					"key41": "val41",
+					"key42": map[string]interface{}{
+						"key421": "val421",
+						"key422": []string{"one", "two", "three"},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			o, err := KVMapToMap(tc.input, tc.prefix)
+			if err != nil {
+				t.Errorf("%s", err)
+			}
+
+			if !reflect.DeepEqual(o, tc.output) {
+				t.Errorf("\nwant:\n%s\nhave:\n%s", tc.output, o)
+			}
+		})
+	}
+
+}
+
+func TestFlattenMapToStruct(t *testing.T) {
+	testCases := []struct {
+		name   string
+		prefix string
+		input  map[string]interface{}
+		output *ST
+	}{
+		{
+			"NestedMapWithPrefix",
+			"",
+			map[string]interface{}{
+				"Key1": "val1",
+				"Key2": 2,
+				"Key3": []int{1, 2, 3},
+				"Key4": map[string]interface{}{
+					"Key41": "val41",
+					"Key42": map[string]interface{}{
+						"Key421": "val421",
+						"Key422": []string{"one", "two", "three"},
+					},
+					"Key43": map[string]interface{}{
+						"Key431": map[string]interface{}{
+							"Key4311": "val4311",
+						},
+					},
+				},
+			},
+			&ST{
+				Key1: "val1",
+				Key2: 2,
+				Key3: []int{1, 2, 3},
+				Key4: &STChildLevel1{
+					Key41: "val41",
+					Key42: map[string]interface{}{
+						"Key421": "val421",
+						"Key422": []string{"one", "two", "three"},
+					},
+					Key43: &STChildLevel2{
+						Key431: map[string]interface{}{
+							"Key4311": "val4311",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			st := &ST{
+				Key4: &STChildLevel1{
+					Key43: &STChildLevel2{},
+				},
+			}
+
+			err := FlattenMapToStruct(tc.input, tc.prefix, st)
+			if err != nil {
+				t.Errorf("%s", err)
+			}
+
+			if !reflect.DeepEqual(st, tc.output) {
+				t.Errorf("\nwant:\n%v\nhave:\n%v", tc.output, st)
+			}
+		})
+	}
+
+}
+
+func TestKVMapToStruct(t *testing.T) {
+	testCases := []struct {
+		name   string
+		prefix string
+		input  map[string]interface{}
+		output *ST
+	}{
+		{
+			"NestedMapWithPrefix",
+			"",
+			map[string]interface{}{
+				"Key1":                      "val1",
+				"Key2":                      2,
+				"Key3/0":                    1,
+				"Key3/1":                    2,
+				"Key3/2":                    3,
+				"Key4/Key41":                "val41",
+				"Key4/Key42/Key421":         "val421",
+				"Key4/Key42/Key422/0":       "one",
+				"Key4/Key42/Key422/1":       "two",
+				"Key4/Key42/Key422/2":       "three",
+				"Key4/Key43/Key431/Key4311": "val4311",
+			},
+			&ST{
+				Key1: "val1",
+				Key2: 2,
+				Key3: []int{1, 2, 3},
+				Key4: &STChildLevel1{
+					Key41: "val41",
+					Key42: map[string]interface{}{
+						"Key421": "val421",
+						"Key422": []string{"one", "two", "three"},
+					},
+					Key43: &STChildLevel2{
+						Key431: map[string]interface{}{
+							"Key4311": "val4311",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			st := &ST{
+				Key4: &STChildLevel1{
+					Key43: &STChildLevel2{},
+				},
+			}
+
+			err := KVMapToStruct(tc.input, tc.prefix, st)
+			if err != nil {
+				t.Errorf("%s", err)
+			}
+
+			if !reflect.DeepEqual(st, tc.output) {
+				t.Errorf("\nwant:\n%v\nhave:\n%v", tc.output, st)
+			}
 		})
 	}
 
